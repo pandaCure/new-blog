9a126076216c6fcb7396b7b7150b2c99cc1988bf
pandaCure <zzyamor@163.com>
2020-04-17 22:55:03 +0800

    feat: add changePage Title component

---
+++ src/components/ChangePageTitle/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+export interface IChangePageTitle {
+  title: string
+}
+const ChangePageTitle = (props: IChangePageTitle) => {
+  const title = props.title
+  return (
+    <Helmet>
+      <title> {title || 'ZhongYao Inc.'}</title>
+    </Helmet>
+  )
+}
+URL
+export default ChangePageTitle
